cb72334286193c7a2c02a953a5698018b1709f64
frrixz135 <114455852+frrixz135@users.noreply.github.com>
2023-03-14 19:11:54 +0700

    Add files via upload

---
+++ week2/index2.php
@@ -0,0 +1,16 @@
+<?php
+
+$nama = $_GET['nama'];
+$matakuliah = $_GET['matkul'];
+$uts = $_GET['uts'];
+$uas = $_GET['uas'];
+$tugas = $['tugas'];
+
+
+echo 'Nama Mahasiswa : '. $nama;
+echo ' <br> Mata Kuliah : '. $matakuliah;
+echo ' <br> Nilai UTS : '. $uts;
+echo ' <br> Nilai UAS : '. $uas;
+echo ' <br> Nilai Tugas : '. $tugas;
+ 
+?>
